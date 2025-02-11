c1b8455ae365c6c47d336a2c14db97d047c54923
João Vitor <joaovitor819oqueres@gmail.com>
2025-02-11 09:35:58 -0300

    lançando-app

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+    let msg = "Svelte + PicoCSS funcionando!";
+  </script>
+  
+  <main class="container">
+    <h1>{msg}</h1>
+    <button>Botão Estilizado</button>
+  </main>
+  
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "@picocss/pico/css/pico.min.css";
+import App from "./App.svelte";
+
+new App({
+  target: document.body,
+});
